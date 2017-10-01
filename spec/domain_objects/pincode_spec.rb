d9b89f16e1579dcb8c37092bbfd4c9080010d801
chapuzzo <luismax@gmail.com>
2017-10-01 14:07:15 +0200

    tested code mandatoriness for pincode creation

--- spec/domain_objects/pincode_spec.rb
+++ spec/domain_objects/pincode_spec.rb
@@ -4,16 +4,26 @@ describe 'Pincode' do
     /^\h{8}-\h{4}-[1-5]\h{3}-[89ab]\h{3}-\h{12}$/
   }
 
-  it 'is not assigned at creation' do
-    created_pincode = Pincode.new code: 'aabbcc'
+  it 'needs a code to be created' do
+    expect{
+      Pincode.new({})
+    }.to raise_error(StandardError, /:code is missing/)
 
-    expect(created_pincode.assigned).to eq(false)
+    expect{
+      Pincode.new code: :some_code
+    }.not_to raise_error
   end
 
   it 'gets an id if not given' do
     created_pincode = Pincode.new code: 'aabbcc'
 
     expect(created_pincode.id).to match(uuid_regex)
+  end
+
+  it 'is not assigned at creation' do
+    created_pincode = Pincode.new code: 'aabbcc'
+
+    expect(created_pincode.assigned).to eq(false)
   end
 
   it 'id must be a valid uuid' do
