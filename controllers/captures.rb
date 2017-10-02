7dd89dd57955a809f39be4e97a182534007f40db
chapuzzo <luismax@gmail.com>
2017-10-02 22:05:23 +0200

    safe conversion to_i for slices

--- controllers/captures.rb
+++ controllers/captures.rb
@@ -40,7 +40,7 @@ Captures = Monster::CRUD.for Repos::Captures, '/captures' do
 
     total_captures_for_image = settings.repo.find(user: params[:user], image: params[:image]).length
 
-    total_tiles = Repos::Sponsor.find(id: :the_sponsor).first.size.values.reduce(:*)
+    total_tiles = Repos::Sponsor.find(id: :the_sponsor).first.size.values.map(&:to_i).reduce(:*)
     ap total_tiles
     halt ok if total_captures_for_image < total_tiles
 
